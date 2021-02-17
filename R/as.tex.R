ede0bcb5f79c9e195d2cfed71dbe267379985627
jeksterslab <social.jeksterslab@gmail.com>
2021-02-17 20:28:25 +0800

    Added R/as.tex.R.

---
+++ R/as.tex.R
@@ -0,0 +1,19 @@
+#' Coerce to LaTeX Math
+#'
+#' The function uses [as.yac_symbol()]
+#' to convert `x` to an object of class `yac_symbol`
+#' and then converts it to LaTeX math
+#' using [Ryacas::tex()].
+#'
+#' @author Ivan Jacob Agaloos Pesigan
+#'
+#' @examples
+#' (x <- matrix(1:10, ncol = 2))
+#' as.tex(x)
+#' @inheritParams is.yac_symbol
+#' @export
+as.tex <- function(x) {
+  return(
+    Ryacas::tex(as.yac_symbol(x))
+  )
+}
